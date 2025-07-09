1acea5e65bd174a5cc93ac46b93970bbde90477d
Kevin <gogamekevin123@gmail.com>
2025-07-09 14:24:15 +0800

    Create repo

---
+++ README.md
@@ -0,0 +1,3 @@
+# nfs-dataset
+Demonstration of a persistent NFS server on Cloudlab, using only 1 NIC
+From https://github.com/lbstoller/nfs-dataset
---
+++ profile.py
@@ -0,0 +1,104 @@
+"""This profile sets up a simple NFS server and a network of clients. The NFS server uses
+a long term dataset that is persistent across experiments. In order to use this profile,
+you will need to create your own dataset and use that instead of the demonstration 
+dataset below. If you do not need persistant storage, we have another profile that
+uses temporary storage (removed when your experiment ends) that you can use. 
+
+Instructions:
+Click on any node in the topology and choose the `shell` menu item. Your shared NFS directory is mounted at `/nfs` on all nodes."""
+
+# Import the Portal object.
+import geni.portal as portal
+# Import the ProtoGENI library.
+import geni.rspec.pg as pg
+# Import the Emulab specific extensions.
+import geni.rspec.emulab as emulab
+
+# Create a portal context.
+pc = portal.Context()
+
+# Create a Request object to start building the RSpec.
+request = pc.makeRequestRSpec()
+
+# Only Ubuntu images supported.
+imageList = [
+    ('urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU20-64-STD', 'UBUNTU 20.04'),
+    ('urn:publicid:IDN+wisc.cloudlab.us+image+dynamicgpu-PG0:c240g5horovod1', 'c240g5horovod1'),
+    ('urn:publicid:IDN+wisc.cloudlab.us+image+dynamicgpu-PG0:c240g5Docker', 'UBUNTU 22.04 cuda12.6'),
+    ('urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU22-64-STD', 'UBUNTU 22.04'),
+    ('urn:publicid:IDN+wisc.cloudlab.us+image+dynamicgpu-PG0:d7525Docker', 'UBUNTU 22.04 cuda12.8'),
+]
+
+# Do not change these unless you change the setup scripts too.
+nfsServerName = "nfs"
+nfsLanName    = "nfsLan"
+nfsDirectory  = "/mydata"
+
+# Number of NFS clients (there is always a server)
+pc.defineParameter("clientCount", "Number of NFS clients",
+                   portal.ParameterType.INTEGER, 2)
+
+pc.defineParameter("phystype",  "Optional physical node type",
+                   portal.ParameterType.NODETYPE, "",
+                   longDescription="Pick a single physical node type (pc3000,d710,etc) " +
+                   "instead of letting the resource mapper choose for you.")
+
+pc.defineParameter("dataset", "Your dataset URN",
+                   portal.ParameterType.STRING,
+                   "urn:publicid:IDN+wisc.cloudlab.us:dynamicgpu-pg0+stdataset+Imagenet1")
+
+pc.defineParameter("osImage", "Select OS image",
+                   portal.ParameterType.IMAGE,
+                   imageList[1], imageList)
+
+# Always need this when using parameters
+params = pc.bindParameters()
+
+# The NFS network. All these options are required.
+nfsLan = request.LAN(nfsLanName)
+nfsLan.best_effort       = True
+nfsLan.vlan_tagging      = True
+nfsLan.link_multiplexing = True
+
+# The NFS server.
+nfsServer = request.RawPC(nfsServerName)
+nfsServer.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU22-64-STD'
+# Attach server to lan.
+nfsLan.addInterface(nfsServer.addInterface())
+# Initialization script for the server
+nfsServer.addService(pg.Execute(shell="sh", command="sudo /bin/bash /local/repository/nfs-server.sh"))
+
+# Special node that represents the ISCSI device where the dataset resides
+dsnode = request.RemoteBlockstore("dsnode", nfsDirectory)
+dsnode.dataset = params.dataset
+
+# Link between the nfsServer and the ISCSI device that holds the dataset
+dslink = request.Link("dslink")
+dslink.addInterface(dsnode.interface)
+dslink.addInterface(nfsServer.addInterface())
+# Special attributes for this link that we must use.
+dslink.best_effort = True
+dslink.vlan_tagging = True
+dslink.link_multiplexing = True
+
+link = request.Lan('link')
+link.link_multiplexing = True
+link.bandwidth = 100000000
+
+# The NFS clients, also attached to the NFS lan.
+for i in range(1, params.clientCount+1):
+    node = request.RawPC("node%d" % i)
+    node.disk_image = params.osImage
+    if params.phystype != "":
+        node.hardware_type = params.phystype
+        pass
+    nfsLan.addInterface(node.addInterface())
+    iface = node.addInterface("interface%d" % i)
+    iface.bandwidth = 100000000
+    link.addInterface(iface)
+    # Initialization script for the clients
+    node.addService(pg.Execute(shell="sh", command="sudo /bin/bash /local/repository/nfs-client.sh"))
+    pass
+
+# Print the RSpec to the enclosing page.
+pc.printRequestRSpec(request)
